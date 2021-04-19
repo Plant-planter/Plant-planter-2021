cffea4438233090b2eaeff5a916780c2f6171f9d
tif-calin <culitif@tuta.io>
2021-04-19 13:58:04 -0700

    add renderGarden dom function

---
+++ dom-utils.js
@@ -0,0 +1,17 @@
+
+
+export function renderGarden(gardenObject) {
+    const div = document.createElement('div');
+    const avatar = document.createElement('img');
+    const anchor = document.createElement('a');
+    const delButton = document.createElement('button');
+
+    avatar.src = gardenObject.avatar;
+    avatar.alt = `${gardenObject.name}'s avatar`;
+    anchor.textContent = gardenObject.name;
+    anchor.href = '../garden/';
+    delButton.textContent = '-';
+
+    div.append(avatar, anchor, delButton);
+    return div;
+}
