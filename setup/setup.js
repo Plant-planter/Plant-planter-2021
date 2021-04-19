f76f45cfe04e219c667ae838e224e0aff09f7a5b
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-04-19 16:52:27 -0700

    setup page garden event listener stuff

--- setup/setup.js
+++ setup/setup.js
@@ -1,10 +1,29 @@
-import { getCurrentGarden, getSpecificGarden } from '../local-storage-utilities.js';
+import { getCurrentGarden, getSpecificGarden, setGarden } from '../local-storage-utilities.js';
 
 const gardenName = getCurrentGarden();
 const gardenObj = getSpecificGarden(gardenName);
 
+const form = document.querySelector('form');
 
+const inputName = document.getElementById('garden-name');
 
+inputName.value = gardenName;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const formInput = formData.get('garden-name');
+    const formSelect = formData.get('location-select');
+    const formRadio = formData.get('avatar');
+
+    gardenObj.avatar = `../assets/${formRadio}`;
+    gardenObj.location = formSelect;
+
+    setGarden(formInput, gardenObj);
+
+    window.location = '../garden/';
+
+});
 
 
 
