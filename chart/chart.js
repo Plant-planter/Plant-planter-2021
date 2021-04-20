a239f0d90896f5b3c8303f74016c396825bf2425
Austin Summerlin <summerlin.austin@gmail.com>
2021-04-20 14:20:51 -0700

    implement chart first attempt

--- chart/chart.js
+++ chart/chart.js
@@ -1,27 +1,36 @@
 import { renderHeaderNav } from '../dom-utils.js';
 import { plants } from '../data/data.js';
+import { getCurrentGarden, getSpecificGarden } from '../local-storage-utilities.js';
 
 renderHeaderNav();
 
-const plantData = JSON.stringify(plants);
+
+
+const gardenData = getSpecificGarden(getCurrentGarden());
 
 var ctx = document.getElementById('harvestChart').getContext('2d');
-
 
 const names = [];
 const minHarvest = [];
 const maxHarvest = [];
 
-for (let booger of plantData) {
-    names.push(booger.commonName);
-    minHarvest.push(booger.minDaysTilHarvest);
-    maxHarvest.push(booger.maxDaysTilHarvest);
+
+for (let row of gardenData.rows) {
+    for (let box of row) {
+        const plantId = box.plant;
+        if (plantId) {
+            const plant = plants[plantId];
+            names.push(plant.commonName);
+            minHarvest.push(plant.minDaysTillHarvest);
+            maxHarvest.push(plant.maxDaysTillHarvest);
+        }
+    }
 }
 
 var harvestChart = new Chart(ctx, { //eslint-disable-line
     type: 'bar',
     data: {
-        labels: name,
+        labels: names,
         datasets: [
             {
                 label: 'Minimum days til harvest',
