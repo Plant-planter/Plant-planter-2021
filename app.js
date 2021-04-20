26c7525226f580ff7639ad636c02d72ca93bb0ca
Casey Cameron <mcasey.cameron@gmail.com>
2021-04-20 13:38:37 -0700

    ph changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // import functions and grab DOM elements
 import { getGardens, createGarden, setCurrentGarden } from './local-storage-utilities.js';
 import { renderGarden } from './dom-utils.js';
-import { getAveragePH } from './utils.js'
+import { getAveragePH, phToColor } from './utils.js'
 
 
 const gardenList = document.querySelector('.gardens');
@@ -41,4 +41,4 @@ form.addEventListener('submit', (e) => {
 
 loadGardens();
 
-getAveragePH();
+phToColor();
--- utils.js
+++ utils.js
@@ -18,5 +18,20 @@ export function getAveragePH() {
     const phMin = 4; //assign this to the garden [object's phMin]
     const phMax = 7; //assign this to the garden [object's phMax]
     const averagePH = (phMin + phMax) / 2;
-    console.log(averagePH);
+    return averagePH;
 }
+
+export function phToColor(phAverage = 7.5) {
+    const phInt = parseInt(phAverage);
+    const phDecimal = (phAverage - Math.floor(phAverage)).toFixed(2); //limits decimal to two places
+    const lightness = 0;
+    const pHColors = {
+        4: 'hsl(81,62%,65%)',
+        5: 'hsl(94,54%,50%)',
+        6: 'hsl(107,52%,43%)',
+        7: 'hsl(136,29%,40%)', 
+        8: 'hsl(162,36%,51%)'
+    };
+    console.log(phInt, phDecimal);
+
+} 
