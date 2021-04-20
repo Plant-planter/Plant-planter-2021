97746ad118cfc76e0987f98f5e77de04e23ee51a
tif-calin <culitif@tuta.io>
2021-04-20 12:12:03 -0700

    save plant in current box

--- garden/garden.js
+++ garden/garden.js
@@ -1,6 +1,6 @@
 // imports
 import { renderHeaderNav } from '../dom-utils.js';
-import { getSpecificGarden, getCurrentGarden } from '../local-storage-utilities.js';
+import { getSpecificGarden, getCurrentGarden, setGarden } from '../local-storage-utilities.js';
 import { plants } from '../data/data.js';
 
 // grab html elements
@@ -8,12 +8,13 @@ const section = document.querySelector('.garden-grid');
 const plantSelector = document.getElementById('plant-selector');
 
 // set up state
+const gardenName = getCurrentGarden();
+let gardenObject = getSpecificGarden(gardenName);
+
 let selectedPlant = ''; // should contain the slug of the plant
 
 // define functions
 function generateGardenGrid() {
-    const gardenName = getCurrentGarden();
-    const gardenObject = getSpecificGarden(gardenName);
     let countRow = 0;
     for (let row of gardenObject.rows){
         countRow++;
@@ -21,7 +22,31 @@ function generateGardenGrid() {
         for (let box of row) {  //each row is an array, each box an object
             countCol++;
             const div = document.createElement('div');
-            div.textContent = `Box ${countRow} - ${countCol}`;
+
+            // if the box was already filled with a plant, load that
+            if (box.plant) {
+                div.textContent = plants[box.plant].commonName;
+            } else {
+                div.textContent = `Box ${countRow} - ${countCol}`;
+            }
+
+            // add event listener for when box is clicked on
+            const nRow = countRow - 1
+            const nCol = countCol - 1
+            div.addEventListener('click', () => {
+                if (selectedPlant) {
+                    // update the state
+                    gardenObject.rows[nRow][nCol].plant = selectedPlant;
+                    setGarden(gardenName, gardenObject);
+                    gardenObject = getSpecificGarden(gardenName);
+
+                    // update the view
+                    const plant = plants[selectedPlant];
+                    div.textContent = plant.commonName;
+                }
+            });
+
+            // append the child
             section.appendChild(div);
         }
     }
@@ -37,7 +62,12 @@ function createPlantOptions() {
     } 
 }
 
-// initialize page and add handlers
+// add event handlers
+plantSelector.addEventListener('change', () => {
+    selectedPlant = plantSelector.value;
+});
+
+// initialize page 
 renderHeaderNav();
 generateGardenGrid();
 createPlantOptions();
