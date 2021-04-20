682a1c3cbc91df143297607c10baf324594c0f35
Casey Cameron <mcasey.cameron@gmail.com>
2021-04-20 15:07:34 -0700

    ph test function

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,7 +1,7 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 import { createGarden } from '../local-storage-utilities.js';
-import { generateGrid } from '../utils.js';
+import { generateGrid, phToColor } from '../utils.js';
 
 const test = QUnit.test;
 
@@ -22,4 +22,18 @@ test('test the createGarden function', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.deepEqual(actual, expected);
+});
+
+test('should return an accurate ph hsla color', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'hsl(136,29%,40%)';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = phToColor(7.0, 7.7);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
 });
