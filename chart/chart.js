4dd194a4ec8b5a95665f42302be98f4a03bfe4bd
Austin Summerlin <summerlin.austin@gmail.com>
2021-04-20 13:19:25 -0700

    implement chart

--- chart/chart.js
+++ chart/chart.js
@@ -1,3 +1,50 @@
 import { renderHeaderNav } from '../dom-utils.js';
+import { plants } from '../data/data.js';
 
 renderHeaderNav();
+
+const plantData = JSON.stringify(plants);
+
+var ctx = document.getElementById('harvestChart').getContext('2d');
+
+
+const names = [];
+const minHarvest = [];
+const maxHarvest = [];
+
+for (let booger of plantData) {
+    names.push(booger.commonName);
+    minHarvest.push(booger.minDaysTilHarvest);
+    maxHarvest.push(booger.maxDaysTilHarvest);
+}
+
+var harvestChart = new Chart(ctx, { //eslint-disable-line
+    type: 'bar',
+    data: {
+        labels: name,
+        datasets: [
+            {
+                label: 'Minimum days til harvest',
+                data: minHarvest,
+                backgroundColor: 'lightpink',
+                borderColor: 'magenta',
+                borderWidth: 1
+
+            },
+            {
+                label: 'Maximum days til harvest',
+                data: maxHarvest,
+                backgroundColor: 'lightblue',
+                borderColor: 'blue',
+                borderWidth: 1
+            }
+        ]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
